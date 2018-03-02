801085b7f2a2969f7e669e2575ee619127f47f3c
MrazTevin <millatevin@gmail.com>
2018-03-02 14:52:28 +0300

    removed spaces in between instagram

--- insta/settings.py
+++ insta/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'instagram ',
+        'NAME': 'instagram',
         'USER':'TevinMilla',
         'PASSWORD':'MariaPassa',
     
