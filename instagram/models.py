e9c8a6c98a41b40c53953aa8dd6bc107ce7672b7
MrazTevin <millatevin@gmail.com>
2018-03-03 13:19:31 +0300

    added tags model with relationship defined

--- instagram/models.py
+++ instagram/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class tags(models.Model):
+    name = models.Charfield(max_length=30)
+    def __str__(self):
+        return self.name
+
 class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_caption = models.CharField(max_length=255)
     comments = models.TextField()
+    tags = models.ManyToManyField(tags)
 
     def __str__(self):
         return self.image_name 
