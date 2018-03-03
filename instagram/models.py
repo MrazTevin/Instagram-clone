ec42c637535a07bb5465e9ef1bcffc474303b8d4
MrazTevin <millatevin@gmail.com>
2018-03-03 13:23:21 +0300

    capitalized letter f

--- instagram/models.py
+++ instagram/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class tags(models.Model):
-    name = models.Charfield(max_length=30)
+    name = models.CharField(max_length=30)
     def __str__(self):
         return self.name
 
