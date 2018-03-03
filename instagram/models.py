cd004435e8a5a1cd1dadce85ea156f1e9f166025
MrazTevin <millatevin@gmail.com>
2018-03-03 13:06:09 +0300

    increased the image caption field size

--- instagram/models.py
+++ instagram/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Image(models.Model):
     image_name = models.CharField(max_length=30)
-    image_caption = models.CharField(max_length=30)
+    image_caption = models.CharField(max_length=255)
     comments = models.TextField()
 
     def __str__(self):
