688271aefb286d83bb4efe423ea9ef4654a15b21
MrazTevin <millatevin@gmail.com>
2018-03-03 12:54:22 +0300

    added a __str__ to return the string representation of our model

--- instagram/models.py
+++ instagram/models.py
@@ -5,4 +5,6 @@ class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_caption = models.CharField(max_length=30)
     comments = models.TextField()
- 
+
+    def __str__(self):
+        return self.image_name 
