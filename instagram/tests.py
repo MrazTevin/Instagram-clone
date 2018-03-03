ecb48e68844038d24f2a6cadeb00a59a6fac9eff
MrazTevin <millatevin@gmail.com>
2018-03-03 13:35:04 +0300

    setup and instance test for images

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from .models import Image,tags
 # Create your tests here.
+def setUp(self):
+    self.family = Image(image_name ='Family',image_caption = 'I love you daddy', comments='You two look amazing in blue')
+
+# testing instance
+def test_instance(self):
+    self.assertTrue(isinstance(self.family,Image))
