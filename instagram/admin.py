86220746fccd06cd0954d1338939a32dd2194a49
MrazTevin <millatevin@gmail.com>
2018-03-04 09:13:58 +0300

    created an image admin to order many to many fields

--- instagram/admin.py
+++ instagram/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Image,tag
 # Register your models here.
 
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tag',)
+
 admin.site.register(Image)
 admin.site.register(tag)
