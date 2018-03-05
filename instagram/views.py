f94018ee7dcaf585808781cb6a90c00e3f614290
MrazTevin <millatevin@gmail.com>
2018-03-05 19:23:53 +0300

    impoted the image model into the view function

--- instagram/views.py
+++ instagram/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Image,tag
 # Create your views here.
 
 def welcome(request):
-    return render(request,'index.html')
+    images = Image.objects.all()
+    return render(request,'index.html',{'images':images})
 
 def home(request):
-    return HttpResponse('lets ride fun with simple insta')
+    return HttpResponse('lets ride fun with simple insta')  
