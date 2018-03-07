d6267281dea3fde766a749234172f5afb9a1b114
MrazTevin <millatevin@gmail.com>
2018-03-07 12:19:35 +0300

    configured url to use the one step workflow

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.con.urls import include
 from .import views 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,7 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$',views.welcome,name='welcome'),
     url(r'^home/$',views.home,name='home'),
-    url(r'^picture/(\d+)',views.picture,name='picture')
+    url(r'^picture/(\d+)',views.picture,name='picture'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
