4a3269e0b0d39fd0fee7a389d2e13081cc2f8d5f
MrazTevin <millatevin@gmail.com>
2018-03-08 11:44:24 +0300

    removed django.con.urls and replaced it with django.conf.urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.con.urls import include
+from django.conf.urls import include
 from .import views 
 from django.conf import settings
 from django.conf.urls.static import static
