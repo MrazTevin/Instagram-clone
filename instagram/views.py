4c50bdd699114bd4e04e200338b73d2f30c79c9f
MrazTevin <millatevin@gmail.com>
2018-03-12 07:37:14 +0300

    added login required function

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . models import Image,tag
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
+@login_required(login_url='/accounts/login')
 def welcome(request):
     images = Image.objects.all()
     return render(request,'index.html',{'images':images})
