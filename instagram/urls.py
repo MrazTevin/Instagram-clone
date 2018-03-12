ba616b646fafe8cf61df695bba78dae2d73c3498
MrazTevin <millatevin@gmail.com>
2018-03-12 07:36:53 +0300

    imported auth views ,added logout url pattern

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import include
 from .import views 
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^$',views.welcome,name='welcome'),
     url(r'^home/$',views.home,name='home'),
     url(r'^picture/(\d+)',views.picture,name='picture'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$',views.logout,{"next_page"':'/'})
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
