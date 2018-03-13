f1ff9d20dd46ed7cc201148f5fb2eca6e3ec95d0
MrazTevin <millatevin@gmail.com>
2018-03-13 14:39:56 +0300

    removed login and replaced it with register

--- instagram/views.py
+++ instagram/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . models import Image,tag
 from django.contrib.auth.decorators import login_required
+from .forms import NewImageForm
 # Create your views here.
 
 
-@login_required(login_url='/accounts/login')
+@login_required(login_url='/accounts/register')
 def welcome(request):
     images = Image.objects.all()
     return render(request,'index.html',{'images':images})
@@ -16,3 +17,18 @@ def home(request):
 def picture(request,image_id):
     picture = Image.objects.get(id = image_id)
     return render(request,'picture.html',{'picture':picture})
+
+
+@login_required(login_url='/accounts/login/')
+def new_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.UserProfile = current_user
+            image.save()
+    else:
+        form = NewImageForm()
+    return render(request, 'new_image.html', {"form": form})
+
