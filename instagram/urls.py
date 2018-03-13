ea563e563ecee579867cff055402c249a5d2196c
MrazTevin <millatevin@gmail.com>
2018-03-13 12:52:51 +0300

    imported views and created a new url pattern for view picture

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,16 +1,15 @@
 from django.conf.urls import url
 from django.conf.urls import include
-from .import views 
+from . import views 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views
+
 
 urlpatterns = [
     url(r'^$',views.welcome,name='welcome'),
     url(r'^home/$',views.home,name='home'),
     url(r'^picture/(\d+)',views.picture,name='picture'),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$',views.logout,{"next_page": '/'})
+    url(r'^new/image$', views.new_picture, name = 'new-picture')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
