28d2e79a990ecab2d0e28317e782c4e2a58e45d0
MrazTevin <millatevin@gmail.com>
2018-03-19 01:42:49 +0300

    beautified my code

--- instagram/models.py
+++ instagram/models.py
@@ -4,10 +4,14 @@ from tinymce.models import HTMLField
 
 User = get_user_model()
 # Create your models here.
+
+
 class tag(models.Model):
     name = models.CharField(max_length=30)
+
     def __str__(self):
         return self.name
+
 
 class Image(models.Model):
     image_name = models.CharField(max_length=30)
@@ -15,14 +19,12 @@ class Image(models.Model):
     comments = models.TextField()
     tags = models.ManyToManyField(tag)
     image = models.ImageField(upload_to='photos/', blank=True)
-    userprofile = models.ForeignKey(User,blank = True, default=1)
+    userprofile = models.ForeignKey(User, blank=True, default=1)
     post = HTMLField()
-   
 
     def __str__(self):
         return self.image_name2
 
-    
     def save_image(self):
         self.save()
 
@@ -32,20 +34,20 @@ class Image(models.Model):
     @classmethod
     def update_caption(cls):
         return cls.objects.update()
-   
+
     @classmethod
     def get_image(cls):
-       return cls.objects.get(id=1)
-   
-class UserProfile(models.Model):
-   profile_photo = models.ImageField(upload_to='photos/', blank = True)
-   first_name = models.CharField(max_length =30)
-   last_name = models.CharField(max_length = 30)
-   email  = models.EmailField()
-   
-   def __str__(self):
-       return self.first_name
-    
-   def save_userprofile(self):
-        self.save()
+        return cls.objects.get(id=1)
 
+
+class UserProfile(models.Model):
+    profile_photo = models.ImageField(upload_to='photos/', blank=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    def save_userprofile(self):
+        self.save()
