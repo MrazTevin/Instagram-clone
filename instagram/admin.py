48dd75b54f429476405e933b5eb8bd5728d5aaaa
MrazTevin <millatevin@gmail.com>
2018-03-19 07:16:18 +0300

    added class comment and registered comment

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from .models import Image,tag,UserProfile
+from .models import Image, tag, UserProfile, Comment
 # Register your models here.
+
 
 class ImageAdmin(admin.ModelAdmin):
     filter_horizontal = ('tag',)
 
+
 admin.site.register(Image)
 admin.site.register(tag)
 admin.site.register(UserProfile)
+admin.site.register(Comment)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = {'user', 'approved'}
+
+
+admin.site.register(Comment, CommentAdmin)
