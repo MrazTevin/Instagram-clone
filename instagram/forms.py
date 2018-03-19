564eb08e9f23a83d6415d24e0a232baff82cdf41
MrazTevin <millatevin@gmail.com>
2018-03-19 10:59:38 +0300

    added class for ther comments

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,16 +1,20 @@
 from .models import Image
 from django import forms
 
+
 class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['userprofile', 'comments', 'tags', 'post']
         widget = {
-            'tags':forms.CheckboxSelectMultiple(),
-            }
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=50)
     file = forms.FileField()
 
-
+# class CommentsForm(forms.ModelForm):
+#     class Meta:
+#         model = comment
